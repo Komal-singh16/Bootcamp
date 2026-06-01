e568565d6fecbf640f1f4c7181c72d642cf4f8e7
Komal-singh16 <engineerkomalchauhan@gmail.com>
2026-06-01 11:47:12 +0530

    Rotate and Invert Case of String

---
+++ String/PermutationsOfString.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class PermutationsOfString {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        char[] ch=str.toCharArray();
+        Arrays.sort(ch);
+        String sorted=new String(ch);
+        do{
+            System.out.println(sorted);
+        }
+        while(next_permut
+    }
+}
